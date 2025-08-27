1209afa3b0af1ef368be8e1e92350d694dd28259
brozerker19 <catalin.constantin.94@gmail.com>
2025-08-27 12:58:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     },
     // Production (Railway)
     production: {
-        apiBaseUrl: 'https://artistic-hope-production.up.railway.app' // ⚠️ REPLACE WITH YOUR ACTUAL RAILWAY URL!
+        apiBaseUrl: 'https://nftemple-live-data-production.up.railway.app/' // ⚠️ REPLACE WITH YOUR ACTUAL RAILWAY URL!
     }
 };
 
